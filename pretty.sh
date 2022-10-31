7e2a2edad067eba826c5f871372fefa7937a00e2
b-van-b <115096508+b-van-b@users.noreply.github.com>
2022-10-31 14:45:33 +0000

    chore: update pretty script

--- pretty.sh
+++ pretty.sh
@@ -1,3 +1,5 @@
 prettier --write server.js
 prettier --write routes/api.js
 prettier --write controllers/sudoku-solver.js
+prettier --write tests/1_unit-tests.js
+prettier --write tests/2_functional-tests.js
