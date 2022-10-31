f1ce5fc3fd2f5ad02a8ba06aa030a7987f1bd10d
b-van-b <115096508+b-van-b@users.noreply.github.com>
2022-10-31 14:59:54 +0000

    test: test outlines

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -14,5 +14,49 @@ const testPuzzle = {
 };
 
 suite("Unit Tests", () => {
+  suite("#validate(puzzleString)", () => {
+    test("Logic handles a valid puzzle string of 81 characters", () => {
+    });
 
+    test("Logic handles a puzzle string with invalid characters (not 1-9 or .)", () => {
+    });
+
+    test("Logic handles a puzzle string that is not 81 characters in length", () => {
+    });
+  });
+
+  suite(
+    "#checkRowPlacement(puzzleString, row, column, value)",
+    () => {
+      test("Logic handles a valid row placement", () => {});
+
+      test("Logic handles an invalid row placement", () => {});
+    }
+  );
+
+  suite(
+    "#checkColPlacement(puzzleString, row, column, value)",
+    () => {
+      test("Logic handles a valid column placement", () => {});
+
+      test("Logic handles an invalid column placement", () => {});
+    }
+  );
+
+  suite(
+    "#checkRegionPlacement(puzzleString, row, column, value)",
+    () => {
+      test("Logic handles a valid region (3x3 grid) placement", () => {});
+
+      test("Logic handles an invalid region (3x3 grid) placement", () => {});
+    }
+  );
+
+  suite("#solve(puzzleString)", () => {
+    test("Valid puzzle strings pass the solver", () => {});
+
+    test("Invalid puzzle strings fail the solver", () => {});
+
+    test("Solver returns the expected solution for an incomplete puzzle", () => {});
+  });
 });
