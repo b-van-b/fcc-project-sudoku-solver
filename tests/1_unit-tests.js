3f0a480c0482923915b810decd36ae627d896bab
b-van-b <115096508+b-van-b@users.noreply.github.com>
2022-10-31 15:16:01 +0000

    test: check row placement

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -6,11 +6,12 @@ const puzzlesAndSolutions =
   require("../controllers/puzzle-strings.js").puzzlesAndSolutions;
 let solver = new Solver();
 const testPuzzle = {
-  string: "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..",
-  row: 'A',
+  string:
+    "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..",
+  row: "A",
   col: 2,
   badValue: 9,
-  goodValue: 6
+  goodValue: 6,
 };
 
 suite("Unit Tests", () => {
@@ -52,32 +53,31 @@ suite("Unit Tests", () => {
     });
   });
 
-  suite(
-    "#checkRowPlacement(puzzleString, row, column, value)",
-    () => {
-      test("Logic handles a valid row placement", () => {});
+  suite("#checkRowPlacement(puzzleString, row, column, value)", () => {
+    test("Logic handles a valid row placement", () => {
+      const result = solver.checkRowPlacement(
+        testPuzzle.string,
+        testPuzzle.row,
+        testPuzzle.col,
+        testPuzzle.goodValue
+      );
+      assert.deepEqual(result, { valid: true });
+    });
 
-      test("Logic handles an invalid row placement", () => {});
-    }
-  );
+    test("Logic handles an invalid row placement", () => {});
+  });
 
-  suite(
-    "#checkColPlacement(puzzleString, row, column, value)",
-    () => {
-      test("Logic handles a valid column placement", () => {});
+  suite("#checkColPlacement(puzzleString, row, column, value)", () => {
+    test("Logic handles a valid column placement", () => {});
 
-      test("Logic handles an invalid column placement", () => {});
-    }
-  );
+    test("Logic handles an invalid column placement", () => {});
+  });
 
-  suite(
-    "#checkRegionPlacement(puzzleString, row, column, value)",
-    () => {
-      test("Logic handles a valid region (3x3 grid) placement", () => {});
+  suite("#checkRegionPlacement(puzzleString, row, column, value)", () => {
+    test("Logic handles a valid region (3x3 grid) placement", () => {});
 
-      test("Logic handles an invalid region (3x3 grid) placement", () => {});
-    }
-  );
+    test("Logic handles an invalid region (3x3 grid) placement", () => {});
+  });
 
   suite("#solve(puzzleString)", () => {
     test("Valid puzzle strings pass the solver", () => {});
