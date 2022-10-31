1d14977dbc7bda4e9996fd9d6b6655c5015e1549
b-van-b <115096508+b-van-b@users.noreply.github.com>
2022-10-31 14:55:57 +0000

    test: add test prerequisites

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,18 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+const Solver = require("../controllers/sudoku-solver.js");
+const puzzlesAndSolutions =
+  require("../controllers/puzzle-strings.js").puzzlesAndSolutions;
+let solver = new Solver();
+const testPuzzle = {
+  string: "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..",
+  row: 'A',
+  col: 2,
+  badValue: 9,
+  goodValue: 6
+};
 
-suite('Unit Tests', () => {
+suite("Unit Tests", () => {
 
 });
