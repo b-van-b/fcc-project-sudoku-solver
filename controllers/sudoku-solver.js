4c982efaf56ae5c9dd70f9906f5a9fc2dccf3fdf
b-van-b <115096508+b-van-b@users.noreply.github.com>
2022-10-31 15:05:45 +0000

    feat: validate puzzle strings with SudokuSolver.validate(puzzleString)

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,5 +1,17 @@
 class SudokuSolver {
-  validate(puzzleString) {}
+  validate(puzzleString) {
+    // returns an object
+    // check for string length
+    if (puzzleString.length != 81) {
+      return { error: "Expected puzzle to be 81 characters long" };
+    }
+    // check for invalid characters
+    if (puzzleString.match(/[^0-9.]/)) {
+      return { error: "Invalid characters in puzzle" };
+    }
+    // otherwise, return success
+    return { success: "Puzzle string is valid" };
+  }
 
   checkRowPlacement(puzzleString, row, column, value) {}
 
