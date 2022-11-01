e47a4429eb14e85d37627359358290094c4ccce5
b-van-b <115096508+b-van-b@users.noreply.github.com>
2022-11-01 14:50:57 +0000

    fix(api): reject missing or unreadable fields posted to /api/check

--- routes/api.js
+++ routes/api.js
@@ -6,19 +6,26 @@ module.exports = function (app) {
   let solver = new SudokuSolver();
 
   app.route("/api/check").post((req, res) => {
-    // reject missing values
-    if (!(req.body.coordinate && req.body.value)) {
+    // reject missing coords, value or puzzle
+    if (!(req.body.coordinate && req.body.value && req.body.puzzle)) {
       return res.json({ error: "Required field(s) missing" });
     }
-    const row = req.body.coordinate.slice(0, 1);
-    const col = req.body.coordinate.slice(1, 2);
+    // row = all letters on the left
+    // col = all digits on the right
+    const row = (req.body.coordinate.match(/^[A-Z]+/i) || [
+      "",
+    ])[0].toUpperCase();
+    const col = (req.body.coordinate.match(/\d+$/i) || [""])[0];
+    // reject if row or col could not be extracted
+    if (!(row && col)) {
+      return res.json({ error: "Invalid coordinate" });
+    }
+    // attempt to read value; reject if non-integer
+    const val = Number(req.body.value);
+    if (!val) return res.json({ error: "Invalid value" });
+    // otherwise, send to solver
     console.log("Coords: " + [row, col]);
-    const result = solver.checkPlacement(
-      req.body.puzzle,
-      row,
-      col,
-      req.body.value
-    );
+    const result = solver.checkPlacement(req.body.puzzle, row, col, val);
     res.json(result);
   });
 
